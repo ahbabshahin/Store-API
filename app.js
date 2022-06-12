26258e742510fd24b44ce4cfd3ec90b18a539848
Ahbab <ahbabshahin7788@gmail.com>
2022-06-13 00:20:45 +0600

    Controller & router has been setup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
-
+const productsRouter = require('./routes/products');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -19,6 +19,7 @@ app.get('/', (req, res) => {
 });
 
 // products routes
+app.use('/api/v1/products', productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -28,8 +29,8 @@ const port = process.env.PORT || 3000;
 const start = async () => {
 	try {
 		// connectDB
-
 		await connectDB(process.env.MONGO_URI);
+
 		app.listen(port, console.log(`Server is listening port ${port}...`));
 	} catch (error) {
 		console.log(error);

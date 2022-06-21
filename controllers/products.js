abcaf11698e8c1b46bc013f9a583b9fac1182665
Ahbab <ahbabshahin7788@gmail.com>
2022-06-21 23:43:59 +0600

    getting product's some property been added(company, price, name)

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,33 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({ featured: true });
+	const search = 'wooden';
+	const products = await Product.find({
+		name: { $regex: search, $options: 'i' },
+	});
 	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-	const { featured } = req.query;
+	const { featured, company, price, name } = req.query;
 	const queryObject = {};
 
 	if (featured) {
 		queryObject.featured = featured === 'true' ? true : false;
 	}
+
+	if (company) {
+		queryObject.company = { $regex: company, $options: 'i' };
+	}
+
+	if (price) {
+		queryObject.price = price;
+	}
+
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i' };
+	}
+
 	console.log(queryObject);
 	const products = await Product.find(queryObject);
 	res.status(200).json({ products, nbHits: products.length });

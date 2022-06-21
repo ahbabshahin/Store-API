f5db94e58531a46efa067ed8de069d5949a6b8f5
Ahbab <ahbabshahin7788@gmail.com>
2022-06-22 00:34:56 +0600

    pagination been added

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,11 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
 	// const products = await Product.find({}).sort('name price');
-	const products = await Product.find({}).select('name price');
+	const products = await Product.find({})
+		.sort('name price')
+		.select('name price')
+		.limit(4)
+		.skip(3);
 
 	res.status(200).json({ products, nbHits: products.length });
 };
@@ -44,6 +48,13 @@ const getAllProducts = async (req, res) => {
 		const fieldList = fields.split(',').join(' ');
 		result = result.select(fieldList);
 	}
+
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+
+	result = result.skip(skip).limit(limit);
+
 	const products = await result;
 	res.status(200).json({ products, nbHits: products.length });
 };

f249ce590eaa0c4642d3ce94b9f852e79a012115
Ahbab <ahbabshahin7788@gmail.com>
2022-06-22 00:14:49 +0600

    select functionality been added

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,14 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({}).sort('name price');
+	// const products = await Product.find({}).sort('name price');
+	const products = await Product.find({}).select('name price');
 
 	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-	const { featured, company, price, name, sort } = req.query;
+	const { featured, company, price, name, sort, fields } = req.query;
 	const queryObject = {};
 
 	if (featured) {
@@ -28,12 +29,20 @@ const getAllProducts = async (req, res) => {
 
 	let result = Product.find(queryObject);
 
+	//sorts
 	if (sort) {
 		const sortList = sort.split(',').join(' ');
 
 		result = result.sort(sortList);
 	} else {
 		result = result.sort('createdAt');
+	}
+
+	//fields(select)
+
+	if (fields) {
+		const fieldList = fields.split(',').join(' ');
+		result = result.select(fieldList);
 	}
 	const products = await result;
 	res.status(200).json({ products, nbHits: products.length });
